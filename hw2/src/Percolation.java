709f51198ccd9f53eb9dbc9cd954703f15ca3d59
Hector <hectorgetfree@gmail.com>
2025-03-10 16:37:58 +0800

    finished the basic code for this task(maybe)

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -2,37 +2,123 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 
 public class Percolation {
-    // TODO: Add any necessary instance variables.
+    private WeightedQuickUnionUF wq;
+    private int[][] grid;
+    private int size;
+    private int openSiteNumber;
 
+    /**
+     * create N-by-N grid, with all sites initially blocked
+     * @param N the size of the grid
+     */
     public Percolation(int N) {
-        // TODO: Fill in this constructor.
+        size = N;
+        grid = new int[size][size];
+        wq = new WeightedQuickUnionUF(N + 2);
+        openSiteNumber = 0;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                grid[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            wq.union(i, N);
+        }
     }
 
+    /**
+     * open the site (row, col) if it is not open already
+     * @param row the according row
+     * @param col the according column
+     */
     public void open(int row, int col) {
-        // TODO: Fill in this method.
+        int presentSite = grid[row][col];
+        if (presentSite == 0) {
+            presentSite = 1;
+            openSiteNumber++;
+        }
+        if (getNearBy(row, col) != -1) {
+            int nearIndex = getNearBy(row, col);
+            int thisIndex = xyTo1D(row, col);
+            wq.union(thisIndex, nearIndex);
+        }
     }
 
+    /**
+     * is the site (row, col) open?
+     * @param row the according row
+     * @param col the according column
+     * @return the status of the site
+     */
     public boolean isOpen(int row, int col) {
-        // TODO: Fill in this method.
-        return false;
+        return grid[row][col] == 1;
     }
 
+    /**
+     * is the site (row, col) full?
+     * @param row the according row
+     * @param col the according column
+     * @return the full status of the size
+     */
     public boolean isFull(int row, int col) {
-        // TODO: Fill in this method.
-        return false;
+        return wq.connected(size * size - 2, grid[row][col]);
     }
 
+    /**
+     * number of open sites
+     * @return number of open sites
+     */
     public int numberOfOpenSites() {
-        // TODO: Fill in this method.
-        return 0;
+        return openSiteNumber;
     }
 
+    /**
+     * does the system percolate?
+     * @return where the system percolate
+     */
     public boolean percolates() {
-        // TODO: Fill in this method.
-        return false;
+        for (int i = 0; i < size; i++) {
+            wq.union(size * size - 1, size * size - 3 - i);
+        }
+        return wq.connected(size * size - 1, size * size - 2);
+
     }
 
-    // TODO: Add any useful helper methods (we highly recommend this!).
-    // TODO: Remove all TODO comments before submitting.
+    /**
+     * find out whether it has nearby open site
+     * @param row the according row
+     * @param col the according col
+     * @return the 1D index of the nearBy, if none return -1
+     */
+    public int getNearBy(int row, int col) {
+        int presentSite = grid[row][col];
+        if (presentSite == 0) {
+            return -1;
+        }
+        // new idea: using two-dimension array to
+        // represent the off-set volume
+        int[][] off = {
+                {-1, 0}, {0, 1},
+                {1, 0}, {0, -1}
+        };
 
+        for (int[] o : off) {
+            int newRow = row + o[0];
+            int newCol = col + o[1];
+
+            if (newRow >= 0 && newRow < size
+                    && newCol >= 0 && newCol < size) {
+                if (grid[newRow][newCol] == 1) {
+                    return xyTo1D(newRow, newCol);
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int xyTo1D(int x, int y) {
+        return size * x + y;
+    }
 }
