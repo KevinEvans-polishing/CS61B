7d7d2cad68d01b946cd5899b830bd2f7122479af
Hector <hectorgetfree@gmail.com>
2025-03-19 19:50:14 +0800

    almost Done but want to refactor my code next step

--- proj2b/src/main/Graph.java
+++ proj2b/src/main/Graph.java
@@ -1,15 +1,18 @@
 package main;
 
 import edu.princeton.cs.algs4.In;
+import org.junit.jupiter.api.Test;
 
 import java.util.*;
+
+import static com.google.common.truth.Truth.assertThat;
 
 /**
  * 利用邻接表构建String有向图
  */
 public class Graph {
     // 底层邻接表
-    List<Integer>[] lists;
+    List<Integer>[] lists = new List[4];
     // 顶点个数
     private int verticesNumber;
     // edge个数
@@ -29,6 +32,7 @@ public class Graph {
     }
 
     public Graph(String file1, String file2) {
+        edgeNumber = 0;
         // 读取类型1 -- 将映射存储到map中
         In in = new In(file1);
         while (in.hasNextLine()) {
@@ -38,7 +42,7 @@ public class Graph {
             String synset = spiltLine[1];
             map.put(id, synset);
         }
-
+        verticesNumber = map.size();
         // 读取类型2 -- 将对应的id关系存储到Graph中
         in = new In(file2);
         while (in.hasNextLine()) {
@@ -53,6 +57,14 @@ public class Graph {
     }
 
 
+    public Graph() {
+        verticesNumber = 4;
+        for (int i = 0; i < 4; i++) {
+            lists[i] = new ArrayList<>();
+        }
+        edgeNumber = 0;
+    }
+
     /**
      * add an edge front-back
      */
@@ -60,6 +72,7 @@ public class Graph {
         // 找到front在邻接表的位置
         // 然后再相应list中添加back
         lists[front].add(back);
+        edgeNumber++;
     }
 
     /**
@@ -83,7 +96,48 @@ public class Graph {
         return edgeNumber;
     }
 
-    public ArrayList<Integer> findListsWhereWordIn(int id) {
-       return null;
+    public ArrayList<Integer> findListsWhereWordIn(String word) {
+        // 找到目标word对应索引
+        ArrayList<Integer> integers = new ArrayList<>();
+        for (int id : map.keySet()) {
+            String node = map.get(id);
+            if (node.contains(word)) {
+                integers.add(id);
+            }
+        }
+        return integers;
+    }
+
+    public Set<Integer> getEverythingFromTheIndexList(int index) {
+        HashSet<Integer> integers = new HashSet<>();
+        integers.add(index);
+        for (int i = 0; i < lists[index].size(); i++) {
+            int n = lists[index].get(i);
+            if (lists[n] != null && !lists[n].isEmpty()) {
+                integers.addAll(getEverythingFromTheIndexList(n));
+            } else {
+                integers.add(n);
+            }
+        }
+        return integers;
+    }
+
+    public Set<String> backToStringSet(Set<Integer> integers) {
+        TreeSet<String> strings = new TreeSet<>();
+        for (int i : integers) {
+            String s = map.get(i);
+            strings.add(s);
+        }
+        return strings;
+    }
+
+    @Test
+    public void getTest() {
+        Graph graph = new Graph();
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+
+        graph.getEverythingFromTheIndexList(0);
     }
 }
--- proj2b/src/main/HyponymSet.java
+++ proj2b/src/main/HyponymSet.java
@@ -13,18 +13,18 @@ public class HyponymSet {
     public static Set<String> getHyponymSet(Graph G, String word) {
         // 1. 得到word对应的邻接表index集合
         // 所以相应的方法应该由Graph API提供
-        List<Integer> indexList = findListsWhereWordIn(G, word);
+        List<Integer> indexList = G.findListsWhereWordIn(word);
         Set<Integer> hyponymsOfInteger = new HashSet<>();
         // 2. 将index和index对应List的所有元素取出
         // 并放入集合中
         // 注意需要递归取出
         // 因为List中元素对应的index所指向的list不一定为空
         for (int index : indexList) {
-            hyponymsOfInteger.addAll(getEverythingfromTheIndexList(G, index));
+            hyponymsOfInteger.addAll(G.getEverythingFromTheIndexList(index));
         }
         // 3. 将id集合映射回word集合
-        //  注意某些id对应的节点不只一个词
+        // 注意某些id对应的节点不只一个词
         // 以及去重问题
-        return backToStringSet(hyponymsOfInteger);
+        return G.backToStringSet(hyponymsOfInteger);
     }
 }
