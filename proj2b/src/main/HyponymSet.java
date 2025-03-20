b89ba614f75aed181d874b5b9ca2e8b79b1e1593
Hector <hectorgetfree@gmail.com>
2025-03-20 19:54:19 +0800

    successfully handled the k != 0 case but kind of slow when getHyponymSet

--- proj2b/src/main/HyponymSet.java
+++ proj2b/src/main/HyponymSet.java
@@ -9,7 +9,32 @@ import java.util.*;
  * 或者说构造函数就是用来找下义词集的
  */
 public class HyponymSet {
-    public static Set<String> getHyponymSet(Graph G, String word) {
+    public static TreeSet<String> getHyponymSet(Graph g, List<String> words) {
+        TreeSet<String> strings = new TreeSet<>();
+        // 遍历每个word得到相应的下义词集
+        // 然后求交集
+        for (String word : words) {
+            if (strings.isEmpty()) {
+                strings = getHyponymSet(g, word);
+            } else {
+                var another = getHyponymSet(g, word);
+                // 注意：
+                // 这里需要用iterator.remove()
+                // 直接调用strings.remove(string)会导致集合在遍历时间
+                // 被修改而报错
+                Iterator<String> iterator = strings.iterator();
+                while (iterator.hasNext()) {
+                    String string = iterator.next();
+                    if (!another.contains(string)) {
+                        iterator.remove();
+                    }
+                }
+            }
+        }
+        return strings;
+    }
+
+    public static TreeSet<String> getHyponymSet(Graph G, String word) {
         // 1. 得到word对应的邻接表index集合
         // 所以相应的方法应该由Graph API提供
         List<Integer> indexList = G.findListsWhereWordIn(word);
@@ -29,7 +54,7 @@ public class HyponymSet {
         return perfectStringSet(setOfString);
     }
 
-    private static Set<String> perfectStringSet(Set<String> origin) {
+    private static TreeSet<String> perfectStringSet(Set<String> origin) {
         TreeSet<String> update = new TreeSet<>();
         for (String string : origin) {
             // 存在空格则进行分割
